79a9f83e8ae87ef22c97495c9206c1cd761e8b45
Stanislas Polu <polu.stanislas@gmail.com>
2011-02-07 16:06:56 +0100

    ENH: SRH action

---
+++ bin/search.js
@@ -0,0 +1,52 @@
+var util = require('util');
+var fwk = require('fwk');
+var cellar = require('cellar');
+
+var cfg = require("./config.js");
+var mongo = require("./mongo.js");
+
+/**
+ * The Search Object
+ * 
+ * Carries on SRH requests
+ * 
+ * @extends {}
+ * 
+ * @param spec {mongo, config}
+ */ 
+var search = function(spec, my) {
+  my = my || {};
+  var _super = {};
+  
+  my.cfg = spec.config || cfg.config;
+  my.mongo = spec.mongo;
+  
+  var that = {};
+  
+  var search;
+  
+  /** cb_(res) */  
+  search = function(pipe, action, cb_) {
+    if(action.targets().length === 0) {
+      action.error(new Error('No target defined'));
+      return;      
+    }
+    if(action.targets().length !== 1) {
+      var msg = 'Multiple targets defined:';
+      for(var i = 0; i < action.targets().length; i ++) {
+	msg += ' ' + action.targets()[i];
+      }
+      action.error(new Error(msg));
+      return;      
+    }
+
+    my.mongo.find(
+      action, 
+      action.targets()[0],
+      action.body(),
+      function(result) {
+	cb_(result);
+      });
+  };
+
+};
